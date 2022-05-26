418fc8c65c7423c8edf790a3547f4c4911390ee5
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2022-05-26 15:10:14 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,9 +13,7 @@ namespace StringCalculator
             Console.WriteLine("Escreva os números que deseja somar: \nObs.: Separe eles por virgulas ou espaços os números.");
             var inputUser = Console.ReadLine();
 
-            separators.Delimiter(inputUser);
-
-            Console.WriteLine("Essa é a soma dos números: " + addNumbers.Add(separators.Delimiter(inputUser).ToArray()));
+            Console.WriteLine($"Essa é a soma dos números: {addNumbers.Add(separators.Delimiter(inputUser).ToArray())}");
         }
     }
 }
